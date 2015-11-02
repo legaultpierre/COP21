79751b9315f8ea52b06f24789c38961318628507
Pierre <pierre.legault64@gmail.com>
2015-11-02 16:59:27 +0100

    First version of the script

--- hyphe_import/import_from_csv.py
+++ hyphe_import/import_from_csv.py
@@ -36,35 +36,60 @@ def main():
   except Exception as e :
     logging.error('Could not initiate connection to hyphe core')
     sys.exit(1)
-
   # Be sure that the corpus is started
   result = hyphe_core.start_corpus(corpus, password)
   if result['code'] == 'fail' :
     logging.error(result['message'])
     sys.exit(1)
+  # Reset the corpus
+  hyphe_core.reinitialize(corpus)
+
+  if result['code'] == 'fail' :
+    logging.error(result['message'])
+  else :
+    logging.info(result['result'])
 
   # Iterate over the web entities
   with open(webentities_file) as f :
     for webentity in csv.DictReader(f) :
+      # Get the necessary info from CSV
       prefixesAsUrls = convertSpaceSeparatedValueToArray(webentity['PREFIXES'])
       name = webentity['NAME']
       status = webentity['STATUS']
       startPages = convertSpaceSeparatedValueToArray(webentity['START PAGES'])
+      wantedId = webentity['ID'];
+
       # Add web entities to corpus
       result = hyphe_core.store.declare_webentity_by_lrus_as_urls(prefixesAsUrls, name, status, startPages, corpus)
-      # # For all web entities, reset the status to the one of the .csv file
-      # result = hyphe_core.store.set_webentity_status(webentity['ID'], webentity['STATUS'], corpus)
-      if result['code'] == 'fail' :
-        logging.error(result['message'])
+      # if result['code'] == 'fail' :
+      #   logging.error(result['message'])
+      # else :
+      #   logging.info(result['result'])
+
+      # Gets the newly created web entity
+      remoteWebEntity = hyphe_core.store.get_webentity_for_url(prefixesAsUrls[0], corpus)
+      if remoteWebEntity['code'] == 'fail':
+        logging.error(remoteWebEntity['message'])
       else :
-        logging.info(result['result'])
-      # Add the web entity to be crawled, if it STATUS is 'IN' and it has not been crawled before, ie. CRAWLING STATUS is 'UNCRAWLED'
-      # if webentity['STATUS'] == 'IN' and webentity['CRAWLING STATUS'] == 'UNCRAWLED' :
-      #   result = hyphe_core.crawl_webentity(webentity['ID'], depth_crawl, phantom_crawl, webentity['STATUS'], 'prefixes', {}, corpus)
-      #   if result['code'] == 'fail' :
-      #     logging.error(result['message'])
-      #   else :
-      #     logging.info(result['result'])
+        # logging.info(remoteWebEntity['result'])
+        remoteWebEntity = remoteWebEntity['result']
+        # Sets the web entity id to the one present in the CSV file
+        result = hyphe_core.store.change_webentity_id(remoteWebEntity['id'], wantedId, corpus)
+        # if result['code'] == 'fail' :
+        #   logging.error(result['message'])
+        # else :
+          # logging.info(result['result'])
+
+      # For all web entities, reset the status to the one of the .csv file
+      result = hyphe_core.store.set_webentity_status(webentity['ID'], webentity['STATUS'], corpus)
+
+      #Add the web entity to be crawled, if it STATUS is 'IN' and it has not been crawled before, ie. CRAWLING STATUS is 'UNCRAWLED'
+      if webentity['STATUS'] == 'IN':
+        result = hyphe_core.crawl_webentity_with_startmode(webentity['ID'], depth_crawl, phantom_crawl, webentity['STATUS'], ["startpages", "prefixes"], {}, corpus)
+        if result['code'] == 'fail' :
+          logging.error(result['message'])
+        # else :
+          # logging.info(result['result'])
 
 def convertSpaceSeparatedValueToArray(string):
   if string == '':
