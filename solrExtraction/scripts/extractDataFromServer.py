3dac9004db315a8dd560f46ae21383274d498a5b
Pierre <pierre.legault64@gmail.com>
2016-01-11 15:31:57 +0100

    Response verifier added

--- solrExtraction/scripts/extractDataFromServer.py
+++ solrExtraction/scripts/extractDataFromServer.py
@@ -5,6 +5,7 @@ import requests
 
 from lxml import etree
 base = 'http://jiminy.medialab.sciences-po.fr/solr/hyphe-cop21-1-new-schema/tvrh?q=*%3A*&fl=text&tv.tf=true&rows=10&start='
+# q=web_entity_id:
 jsonObject = {}
 
 def generateURLs(base, lastCall, step):
@@ -14,35 +15,50 @@ def generateURLs(base, lastCall, step):
     urls.append(base + str(i))
   return urls
 
-def indexContent(url):
+def verifyRequest(url, it, max):
   response = requests.get(url)
+  if (response.status_code == 200):
+    return response
+  elif (it < max):
+    time.sleep(2)
+    return verifyRequest(url, it + 1, max)
+  else :
+    return -1
 
-  tree = etree.fromstring(response.content)
-  # tree = etree.parse('../tvrh.xml')
+def indexContent(url):
+  response = verifyRequest(url, 0, 20)
+  if (response == -1):
+    print('ERROR: ' + url)
+  else :
+    tree = etree.fromstring(response.content)
+    # tree = etree.parse('../tvrh.xml')
 
 
-  # print tree.xpath('/response//lst[@name="termVectors"]/lst')
-  index = 0
-  for page in tree.xpath('/response//lst[@name="termVectors"]/lst'):
-    pageObject = {}
-    url = '/response//lst[@name="termVectors"]'
-    namePage = page.get('name')
-    url = url + '//lst[@name="' + namePage + '"]//lst[@name="text"]/lst'
+    # print tree.xpath('/response//lst[@name="termVectors"]/lst')
+    index = 0
+    for page in tree.xpath('/response//lst[@name="termVectors"]/lst'):
+      pageObject = {}
+      url = '/response//lst[@name="termVectors"]'
+      namePage = page.get('name')
+      url = url + '//lst[@name="' + namePage + '"]//lst[@name="text"]/lst'
 
-    listOfWordElements = tree.xpath(url)
-    wordsObject = {}
-    for wordElement in listOfWordElements:
-      word =  wordElement.get('name')
-      value = int(wordElement.getchildren()[0].text)
-      wordsObject[word] = value
-    index = index + 1
-    pageObject['words'] = wordsObject
-    jsonObject[namePage] = pageObject
+      listOfWordElements = tree.xpath(url)
+      wordsObject = {}
+      for wordElement in listOfWordElements:
+        word =  wordElement.get('name')
+        value = int(wordElement.getchildren()[0].text)
+        wordsObject[word] = value
+      index = index + 1
+      pageObject['words'] = wordsObject
+      jsonObject[namePage] = pageObject
 
 def export():
   with open('../extractedData/extractedWords.json', 'w') as outfile:
       json.dump(jsonObject, outfile)
 
+i = 0
 for url in generateURLs(base, 4790, 10):
+  print i
   indexContent(url)
+  i = i + 1
 export()
