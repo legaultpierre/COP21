802b29a282547c104ef6deed92c59262a9cd5a4a
Pierre <pierre.legault64@gmail.com>
2016-01-19 10:06:39 +0100

    Sorted words now stored

--- solrExtraction/scripts/mostEmployedWords.py
+++ solrExtraction/scripts/mostEmployedWords.py
@@ -1,61 +1,54 @@
-# utils
-import re, sys
-
-# data sources
-import sunburnt
+import sys
 import json
+from os import listdir
+from os.path import isfile, join
 
-# Creates an object that will contain all the data retrieved
-index = {}
+# Gets all the files at the given folderPath
+def getFiles(folderPath):
+  return [f for f in listdir(folderPath) if isfile(join(folderPath, f))]
 
-# Contains all the Solr, Hyphe and Mongo conf
-conf = {}
+# Gets the data from the given folder
+def loadData(file):
+  with open(file) as f :
+    json_data=f.read()
+    return json.loads(json_data)
 
-# Contains the solr method to execute
-solrCriteria = {
-  'language': 'EN'
-}
+# Returns the second argument of a list
+def getCount(wordObject):
+  return wordObject[1]
 
-# Translate the solr criteria object into a valid sunburnt request
-def translateToSunburnt(solr, solrCriteria):
-  return solr.query(**solrCriteria)
+# Returns the a list of ['word', count] sorted by descending count
+def sortWords(words):
+  list = []
+  for word in words:
+    wordObject = []
+    wordObject.append(word)
+    wordObject.append(words[word])
+    list.append(wordObject)
+  return sorted(list, key=getCount, reverse=True)
 
-# Loads the Solr, Hyphe, Mongo configuration into conf
-def loadConf():
-  #Load conf
-  try:
-    with open('../../hyphe2solr_modified/config.json') as confile:
-      conf = json.loads(confile.read())
-      return conf
-  except Exception as e:
-    sys.stderr.write("%s: %s\n" % (type(e), e))
-    sys.stderr.write('ERROR: Could not read configuration\n')
-    sys.exit(1)
+# Exports the result to a file
+def export(jsonObject, fileName):
+  with open('../extractedData/topWords/' + fileName, 'w') as outfile:
+      json.dump(jsonObject, outfile)
 
-# Calls the given solr method
-def getSolrResults(conf, solrCriteria):
-  adress = "http://%s:%s/solr/%s" % \
-    (conf['solr']['host'], conf['solr']['port'],
-    get_solr_instance_name(conf['solr']['path']))
-  try:
-    solr = sunburnt.SolrInterface(adress)
-  except Exception as e:
-    sys.stderr.write("%s: %s\n" % (type(e), e))
-    sys.stderr.write('ERROR: Could not initiate connection to SOLR node\n')
-    sys.exit(1)
-
-  try:
-    result = translateToSunburnt(solr, solrCriteria).execute()
-    return result
-  except Exception as e:
-    sys.stderr.write("%s: %s\n" % (type(e), e))
-    sys.stderr.write('ERROR: Could retrieve data from Solr\n')
-    sys.exit(1)
-
-# utils
-re_solrname = re.compile(r"^.*/([^/]+)$")
-get_solr_instance_name = lambda solrpath: re_solrname.sub(r"\1", solrpath)
+def main(modeNumber):
+  if (modeNumber == 0):
+    mode = 'indexedByTag'
+  else:
+    mode = 'indexesByWE'
+  folder = '../extractedData/' + mode + '/'
+  files = getFiles(folder)
+  for f in files:
+    fileData = loadData(folder + f)
+    words = fileData['joinedData']
+    sortedWords = sortWords(words)
+    export(sortedWords, mode + '/' + f)
 
 if __name__=='__main__':
-  conf = loadConf()
-  print getSolrResults(conf, solrCriteria)
+  arguments = sys.argv
+  mode = 0
+  if (len(arguments) == 2):
+    if (arguments[1] > 1):
+      mode = 1
+  main(mode)
