6d029bf56abc7886b2ffb8219b9c782a8ae6b7e6
Pierre <pierre.legault64@gmail.com>
2016-01-25 16:02:49 +0100

    Word filtering now better

--- solrExtraction/scripts/linkWEbyWordCooccurence.py
+++ solrExtraction/scripts/linkWEbyWordCooccurence.py
@@ -1,7 +1,28 @@
 import sys
+import csv
 import json
+import os
 from os import listdir
 from os.path import isfile, join
+
+webentities_file = 'COP21.csv'
+
+def createWeList():
+  weList = []
+  # Iterate over the web entities
+  with open(webentities_file) as f :
+    i = 0
+    for webentity in csv.DictReader(f) :
+      i = i + 1
+      # Get the necessary info from CSV
+      id = webentity['ID']
+      name = webentity['NAME']
+      status = webentity['STATUS']
+
+      if webentity['STATUS'] == 'IN':
+        weList.append(id)
+
+  return weList
 
 # Gets all the files at the given folderPath
 def getFiles(folderPath):
@@ -26,34 +47,50 @@ def indexWords(we, newData, wordIndex):
       wordIndex[word]['count'] += count
       wordIndex[word]['wes'][we] = count
 
-def filterWordIndex(wordIndex, maximalCount, minimalCount, rateMax, rateMin):
+def filterWordIndex(wordIndex, weList, maximalCount, minimalCount, rateMax, rateMin):
   newIndex = {}
+  maxLinks = (len(weList)*rateMax)
+  minLinks = (len(weList)*rateMin)
   for word in wordIndex:
     # Filters by the occurence count
-    if ((wordIndex[word]['count'] < maximalCount) and
-        (wordIndex[word]['count'] > minimalCount) and
-        (len(wordIndex[word]['wes']) > 1 and
-        (len(wordIndex[word]['wes']) <= (len(wordIndex)*rateMax)) and
-        (len(wordIndex[word]['wes']) >= (len(wordIndex)*rateMin))):
-      newIndex[word] = wordIndex[word]
+    if ((wordIndex[word]['count'] <= maximalCount) and
+        (wordIndex[word]['count'] >= minimalCount)):
+      wesNumber = (len(wordIndex[word]['wes']))
+      if (
+          wesNumber <= maxLinks and
+          wesNumber >= minLinks
+        ):
+        print word
+        print '%f, %f, %d' % (minLinks, maxLinks, (len(wordIndex[word]['wes'])))
+        newIndex[word] = wordIndex[word]
   return newIndex
 
 def export(jsonObject, fileName):
-  with open('../extractedData/' + fileName + '.json', 'w') as outfile:
+  path = '../extractedData/wordIndexLinksWithWE/'
+  if not os.path.exists(path):
+    os.makedirs(path)
+  with open(path + fileName + '.json', 'w') as outfile:
     json.dump(jsonObject, outfile)
 
 
 def main():
+  weList = createWeList()
+  print weList
   folder = '../extractedData/topWords/indexesByWE/'
   files = getFiles(folder)
   index = {}
   wordIndex = {}
+  maxCount = 200000000
+  minCount = 5000
+  maxRate = 0.5
+  minRate = 0.0
   for f in files:
     we = f[:-5]
     index[we] = loadData(folder + f)
     indexWords(we, index[we], wordIndex)
-  wordIndex = filterWordIndex(wordIndex, 600000, 5000, 1, 0.8)
-  export(wordIndex, 'wordIndexLinksWithWE')
+  wordIndex = filterWordIndex(wordIndex, weList, maxCount, minCount, maxRate, minRate)
+  fileName = 'wordIndexLinksWithWE_%d_%d_%f_%f' % (maxCount, minCount, maxRate, minRate)
+  export(wordIndex, fileName)
 
 
 if __name__=='__main__':
